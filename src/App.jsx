a588b92b63bcdfe52723b091415cfccdd6f50ce5
Ruben Ramirez <rubenramirez@Rubens-MBP.lan>
2023-02-07 23:14:04 -0500

    completed navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+
+//imports
+import Navbar from "./components/Navbar.jsx";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="App">
-
+      <Navbar />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <img
+        src="../../public/travel_planner_logo.png"
+        width="140px"
+        height="24px"
+      />
+    </div>
+  );
+};
+
+export default Navbar;
